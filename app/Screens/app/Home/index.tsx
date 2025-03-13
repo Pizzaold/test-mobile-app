0b95dd4e610cf83779e3cfc1fc80621db38db724
Pizzaold <109032167+Pizzaold@users.noreply.github.com>
2025-03-13 11:04:10 +0200

    Categories

--- app/Screens/app/Home/index.tsx
+++ app/Screens/app/Home/index.tsx
@@ -1,14 +1,26 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Image, FlatList } from "react-native";
 import styles from "./style";
 import Header from "@/components/Header";
+import categories from "@/utils/categories";
+import CategoryBox from "@/components/CategoryBox";
 
 export default function Home() {
+
+    const renderCategoriItem = (item: any, index: number) => {
+        return (
+            <CategoryBox title={item.title} image={item.image} />
+        )
+    }
 
     return (
         <SafeAreaView>
             <View style={styles.container}>
                 <Header title="Find All You Need" showBack={false} showSearch={true} />
-                <Text>Home</Text>
+                <FlatList
+                    style={styles.list} horizontal showsHorizontalScrollIndicator={false}
+                    data={categories}
+                    renderItem={({item, index}) => renderCategoriItem(item, index)}
+                />
             </View>
         </SafeAreaView>
     )
